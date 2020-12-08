c4404bfa385b5dce532a4473cc0e4367baedeea7
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-09 00:14:07 +0200

    removing unnecessary page

--- layout/menu.php
+++ layout/menu.php
@@ -2,5 +2,4 @@
     <li><a href="index.php">Начало</a></li>
     <li><a href="reservations.php">Резервации</a></li>
     <li><a href="my-reservations.php">Мои резервации</a></li>
-    <li><a href="admin-reservations.php">Направени резервации</a></li>
 </ul>
