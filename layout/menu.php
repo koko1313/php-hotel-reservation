98598863aa8a601df29a2846e60dcb3e9de0c899
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-09 23:25:25 +0200

    Adding styles to the menu

--- layout/menu.php
+++ layout/menu.php
@@ -1,5 +1,21 @@
-<ul>
-    <li><a href="index.php">Начало</a></li>
-    <li><a href="reservations.php">Резервации</a></li>
-    <li><a href="my-reservations.php">Мои резервации</a></li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Начало</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="reservations.php">Резервации</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="my-reservations.php">Мои резервации</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
