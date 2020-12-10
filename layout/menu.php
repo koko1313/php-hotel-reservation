5348a0ee2e08d7e6727d49339c12a08adb45ae91
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-10 23:25:39 +0200

    Menu fixing depending on logged or non logged user

--- layout/menu.php
+++ layout/menu.php
@@ -5,37 +5,32 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <?php if (isset($_SESSION['user'])){ ?>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="index.php">Начало</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="reservations.php">Резервации</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="my-reservations.php">Мои резервации</a>
-                </li>
+                <?php if (isset($_SESSION["user"])) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="reservations.php">Резервации</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="my-reservations.php">Мои резервации</a>
+                    </li>
+                <?php } ?>
             </ul>
-            <ul class="navbar-nav navbar-right"> 
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="logout.php">Изход</a>
-                </li>
-            </ul>
-            <?php } else { ?>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Начало</a>
-                </li>
-                </ul>
-                <ul class="navbar-nav navbar-right">         
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="registration.php">Регистрация</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="login.php">Вход</a>
-                </li>
-            <?php };?>
+            <ul class="navbar-nav navbar-right">
+                <?php if (isset($_SESSION['user'])){ ?>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="logout.php">Изход</a>
+                    </li>
+                <?php } else{ ?>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="registration.php">Регистрация</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="login.php">Вход</a>
+                    </li>
+                <?php };?>
             </ul>
         </div>
     </div>
