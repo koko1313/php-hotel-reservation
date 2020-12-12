23621908d1fb0f8177c3dec1ce1e0d67448b0e49
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-13 00:05:31 +0200

    Adding styles to my-reservations page

--- my-reservations.php
+++ my-reservations.php
@@ -4,8 +4,6 @@
 
     <?php redirectIfNotLogged() ?>
 
-    <h1>Мои резервации</h1>
-
     <?php 
         $results = $db->query("
             SELECT *
@@ -14,23 +12,26 @@
         ");
     ?>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Резервация №</th>
-                <th scope="col">Стая №</th>
-                <th scope="col">Тип на стаята</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($reservation = mysqli_fetch_array($results)) { ?>
+    <div class="container main-content">
+        <h1>Мои резервации</h1>
+        <table class="table">
+            <thead>
                 <tr>
-                    <th scope="row"><?php echo $reservation["reservationid"] ?></th>
-                    <td><?php echo $reservation["roomnumber"] ?></td>
-                    <td><?php echo $reservation["roomtype"] ?></td>
+                    <th scope="col">Резервация №</th>
+                    <th scope="col">Стая №</th>
+                    <th scope="col">Тип на стаята</th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php while($reservation = mysqli_fetch_array($results)) { ?>
+                    <tr>
+                        <th scope="row"><?php echo $reservation["reservationid"] ?></th>
+                        <td><?php echo $reservation["roomnumber"] ?></td>
+                        <td><?php echo $reservation["roomtype"] ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 
 <?php include "layout/footer.php" ?>
