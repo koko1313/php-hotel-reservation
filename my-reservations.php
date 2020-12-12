bd737349fb2d033b6aa49d602fc57ad2534801f7
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-12 23:56:42 +0200

    Show current logged user reservations

--- my-reservations.php
+++ my-reservations.php
@@ -6,4 +6,31 @@
 
     <h1>Мои резервации</h1>
 
+    <?php 
+        $results = $db->query("
+            SELECT *
+            FROM reservationview
+            WHERE clientid = '". $_SESSION["user"]["id"] ."'
+        ");
+    ?>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Резервация №</th>
+                <th scope="col">Стая №</th>
+                <th scope="col">Тип на стаята</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($reservation = mysqli_fetch_array($results)) { ?>
+                <tr>
+                    <th scope="row"><?php echo $reservation["reservationid"] ?></th>
+                    <td><?php echo $reservation["roomnumber"] ?></td>
+                    <td><?php echo $reservation["roomtype"] ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
 <?php include "layout/footer.php" ?>
