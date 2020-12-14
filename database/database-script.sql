b9f7aa4a7dd76e347edec0a731d00e5626731505
Ivan <theflowzon3@gmail.com>
2020-12-14 21:31:16 +0200

    changed client to user in the database

--- database/database-script.sql
+++ database/database-script.sql
@@ -12,7 +12,7 @@ CREATE TABLE room(
 	FOREIGN KEY (roomtypeid) REFERENCES roomtype(id)
 )ENGINE=INNODB;
 
-CREATE TABLE client(
+CREATE TABLE user(
 	id int unsigned PRIMARY KEY AUTO_INCREMENT,
 	firstname varchar (20),
 	lastname varchar (20),
@@ -26,10 +26,10 @@ CREATE TABLE client(
 
 CREATE TABLE reservation (
 	id int unsigned PRIMARY KEY AUTO_INCREMENT,
-	clientid int unsigned,
+	userid int unsigned,
 	roomid int unsigned,
 	`date` date,
-	FOREIGN KEY(clientid) REFERENCES client(id),
+	FOREIGN KEY(userid) REFERENCES user(id),
 	FOREIGN KEY(roomid) REFERENCES room(id)
 )ENGINE=INNODB;
 
@@ -59,21 +59,21 @@ INNER JOIN roomtype ON room.roomtypeid=roomtype.id;
 CREATE VIEW reservationview AS
 SELECT
 	reservation.id AS reservationid,
-	client.id AS clientid,
-    client.firstname,
-    client.lastname,
-    client.preferredroombedcount,
+	user.id AS userid,
+    user.firstname,
+    user.lastname,
+    user.preferredroombedcount,
 	roomtype.id AS roomtypeid,
     roomtype.roomtype,
 	room.id AS roomid,
     room.roomnumber
 FROM reservation
-INNER JOIN client ON reservation.clientid=client.id
+INNER JOIN user ON reservation.userid=user.id
 INNER JOIN room ON reservation.roomid=room.id
 INNER JOIN roomtype ON room.roomtypeid=roomtype.id;
 
 ALTER DATABASE hotel CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE client CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE user CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE reservation CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE room CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE roomtype CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
