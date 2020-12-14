1158b60df424540922116ee301d7e98b0524d124
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-14 23:21:22 +0200

    adding favicon

--- layout/header.php
+++ layout/header.php
@@ -7,8 +7,9 @@
 <!doctype html>
 <html>
 <head>
-    <title>Хотел</title>
+    <title>Хотел Медуза</title>
     <meta charset="UTF-8">
+    <link rel="icon" href="picture/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
