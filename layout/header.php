41959219339156ccb64f9aed209590afd5e07c0f
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-16 16:03:36 +0200

    Adding metatag for viewport
    this is needed for the mobile design

--- layout/header.php
+++ layout/header.php
@@ -9,6 +9,7 @@
 <head>
     <title>Хотел Медуза</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="picture/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
