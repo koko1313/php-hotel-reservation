13cb4422c531e2857063eaefd390a37685bf836a
koko1313 <kaloyanvellichkov@gmail.com>
2020-12-25 10:34:28 +0200

    Adding snow

--- layout/header.php
+++ layout/header.php
@@ -19,6 +19,7 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script src="scripts/form-validations.js"></script>
+    <script src="scripts/snowstorm-min.js"></script>
 </head>
 <body class="d-flex flex-column min-vh-100">
 
